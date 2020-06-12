57a194806a87c56ffc15ee42be0e8f9d46863e36
benlo <benlo.benlong550@gmail.com>
2020-06-12 13:45:08 -0700

    Created GridView of 10 x 20 Unit Snake Map.  Initialized Snake Body and Fuji Apple Positions and Color

--- lib/pages/Snake.dart
+++ lib/pages/Snake.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'dart:async';
+//import 'dart:math';
+//import 'dart:async';
 
 
 
@@ -22,24 +22,77 @@ class SnakeApp extends StatefulWidget {
 
 class _SnakeAppState extends State<SnakeApp> {
 
-  final int numSquaresRow = 20;
-  final int numSquaresCol= 40;
+  final int numSquaresRow = 10;       //grid size dimensions
+  final int numSquaresCol= 20;
+  var snake = [[0,1], [0,0]];         //snake start position of size 2 units
+  var apple = [0, 5];                 //start pos of food
+  bool inGame = false;                //flag signals movement
+  var dir = "down";                   //movement direction
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: RichText(text: TextSpan(  
-          style: DefaultTextStyle.of(context).style,
-          children: <TextSpan> [
-            TextSpan(  
-              text: "Snake Coming Soon. \n",
-              style: TextStyle(color: Colors.white)
+     backgroundColor: Colors.brown[400],
+     body: Column(
+       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+       crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          child: GestureDetector(
+            child: AspectRatio(
+              aspectRatio: numSquaresRow / numSquaresCol,             //Aspect Ratio: width / height , uses double
+              child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: numSquaresRow,
+                ),
+                itemCount: numSquaresRow * numSquaresCol,
+                itemBuilder: (BuildContext context, int index) {
+
+                  bool isBody = false;
+
+                  var x = index % numSquaresRow;
+                  var y = (index / numSquaresRow).floor();
+
+                  //Identifying snake body
+                  for (var pos in snake){
+                    if ( pos[0] == x && pos[1] == y){
+                      isBody = true;
+                      break;
+                    }
+                  }
+
+                  //Applying color to snake body, apple, and map
+                  var color;
+                  if (snake.first[0] == x && snake.first[1] == y) {
+                    color = Colors.greenAccent;
+                  }
+                  else if(isBody) {
+                    color = Colors.green[200];
+                  }
+                  else if(apple[0] == x && apple[1] == y) {
+                    color = Colors.red;
+                  }
+                  else{
+                    color = Colors.grey[200];
+                  }
+
+                  //return container for build
+                  return Container(
+                    margin: EdgeInsets.all(1),
+                    decoration: BoxDecoration(  
+                      color: color,
+                      //color: Colors.yellow[100],
+                      shape: BoxShape.circle,
+                    ),
+                  );
+                }
+              ),
             )
-          ]
-        ),)
-      )
+          )
+        )
+       ],
+     )
     );
   }
 }
